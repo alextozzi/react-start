8e7c1afe225017f1372e6565b9e4517a8ca850d2
alextozzi <alexandre.tozzi@gmail.com>
2017-12-29 21:44:59 +0100

    start files work now

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { App } from './components/App'
-import { First} from './components/First'
-import { Router, Route, hashHistory } from 'react-router'
 
 window.React = React
 
 render(
-    <Router history={hashHistory}>
-        <Route path="/" component={App}>
-            <Route path="/first" componet={First} />
-        </Route>
-    </Router>,
+    <App />,
     document.getElementById('react-container')
 )
-
